1e623356390cf86befe9dff06dfbfcd90204f2ac
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-07 00:52:01 +0200

    add style to register screen

--- app/screens/register/register.screen.tsx
+++ app/screens/register/register.screen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import { Appbar, Button, TextInput } from "react-native-paper";
+import { registerStyle } from "./register.style";
 
 export const RegisterScreen = () => {
 
@@ -13,13 +14,14 @@ export const RegisterScreen = () => {
                 <Appbar.BackAction/>
                 <Appbar.Content title='Register'/>
             </Appbar>
-
-            <TextInput label='Name'/>
-            <TextInput label= 'Email' keyboardType='email-address'/>
-            <TextInput label='Password' secureTextEntry={true} right= {<TextInput.Icon name="eye-off-outline"/>} />
-            <TextInput label='Confirm Password ' secureTextEntry={true} right= {<TextInput.Icon name="eye-off-outline"/>}/>
-            <TextInput label='Phone Number' keyboardType='number-pad'/>
-            <Button>Register</Button>
+            <View style={registerStyle.content}>
+                <TextInput label='Name'/>
+                <TextInput label= 'Email' keyboardType='email-address'/>
+                <TextInput label='Password' secureTextEntry={true} />
+                <TextInput label='Confirm Password ' secureTextEntry right={<TextInput.Icon name="eye-off-outline" />}/>
+                <TextInput label='Phone Number' keyboardType='number-pad'/>
+                <Button mode='contained' style={registerStyle.button}>Register</Button>
+             </View>
 
          </ScrollView>
         
---
+++ app/screens/register/register.style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const registerStyle = StyleSheet.create({
+    content: {
+        padding: 20,
+        paddingTop: 0
+    },
+
+    button: {
+        marginTop: 30,
+        padding:8
+    }
+
+
+})
