897a559d8842949da8bcb53d175cf6f348abc296
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-06 02:39:16 +0200

    finished style the login card

---
+++ App.style.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "react-native-paper";
+
+export const them = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#4682b4',
+        background: "transparent"
+    }
+}
--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import  React from 'react';
 
 
 import { Provider as PaperProvider, TextInput} from 'react-native-paper';
+import { them } from './App.style';
 import { LoginScreen } from './app/screens/login/login.screen';
 
 const App = () => {
 
 
   return(
-  <PaperProvider>
+  <PaperProvider theme={them}>
 
     <LoginScreen/>
   </PaperProvider>
