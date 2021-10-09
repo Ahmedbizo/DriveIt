75f9a87fcc3a005f62ab71ffcb957f88c59bd5c8
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-10 00:52:42 +0200

    added maps to the home screen

--- app/screens/home/home.screen.tsx
+++ app/screens/home/home.screen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, View , StyleSheet} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; 
 
 
 const HomeScreen = () => {
@@ -8,9 +9,34 @@ const HomeScreen = () => {
 
         <SafeAreaView>
 
-            <Text> Home Screen </Text>
+<View style={styles.container}>
+     <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
         </SafeAreaView>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
 
 export default HomeScreen;
