777eda794d26ad378c417fd2ed95fc5417e5f1dd
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-10 01:02:33 +0200

    added style to home screen

--- app/screens/home/home.screen.tsx
+++ app/screens/home/home.screen.tsx
@@ -10,7 +10,15 @@ const HomeScreen = () => {
         <SafeAreaView style={{ flex: 1}}>
 
 
-     <MapView style={{flex: 1}}/>
+     <MapView style={{flex: 1 }}
+     region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+     
+     />
      
      
    
