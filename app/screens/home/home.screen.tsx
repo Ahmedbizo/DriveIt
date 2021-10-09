ebe0f5d17a24c6d5a9bacc0e3630ffa851798c9d
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-09 23:46:12 +0200

    added home screen page

---
+++ app/screens/home/home.screen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+
+const HomeScreen = () => {
+
+    return (
+
+        <SafeAreaView>
+
+            <Text> Home Screen </Text>
+        </SafeAreaView>
+    )
+};
+
+export default HomeScreen;
