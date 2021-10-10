c68aa10eb1345e3fabd839fe31756d373dc944fe
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-10 02:01:48 +0200

    added state to the home screen

--- app/screens/home/home.screen.tsx
+++ app/screens/home/home.screen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Image, SafeAreaView} from 'react-native';
 import MapView, { Marker } from 'react-native-maps'; 
-import { FAB } from 'react-native-paper';
+import { Card, FAB,IconButton,List } from 'react-native-paper';
 import { homeStyle } from './home.style';
 
 
 const HomeScreen = () => {
+
+    const state: number = 2;
 
     return (
 
@@ -20,45 +22,85 @@ const HomeScreen = () => {
         longitudeDelta: 0.0121,
       }}>
 
-            <Marker 
-                    description="Delivery"
-                    coordinate={{  latitude: 37.78819,longitude: -122.4315}}>
-                    <Image 
-                    style={homeStyle.markerImage}
-                    source={ require ("../../../assets/car.png")}/>
+      {
+          state == 1 ? 
 
-          </Marker>
+          <>
 
           <Marker 
-                    description="Delivery"
-                    coordinate={{  latitude: 37.78821,longitude: -122.4321}}>
-                    <Image 
-                    style={homeStyle.markerImage}
-                    source={ require ("../../../assets/car.png")}/>
+          description="Delivery"
+          coordinate={{  latitude: 37.78819,longitude: -122.4315}}>
+          <Image 
+          style={homeStyle.markerImage}
+          source={ require ("../../../assets/car.png")}/>
 
-          </Marker>
-          <Marker 
-                    description="Delivery"
-                    coordinate={{  latitude: 37.78845,longitude: -122.4340}}>
-                    <Image 
-                    style={homeStyle.markerImage}
-                    source={ require ("../../../assets/car.png")}/>
+        </Marker>
 
-          </Marker>
-          <Marker 
-                    description="Delivery"
-                    coordinate={{  latitude: 37.78855,longitude: -122.4355}}>
-                    <Image 
-                    style={homeStyle.markerImage}
-                    source={ require ("../../../assets/car.png")}/>
+        <Marker 
+                description="Delivery"
+                coordinate={{  latitude: 37.78821,longitude: -122.4321}}>
+                <Image 
+                style={homeStyle.markerImage}
+                source={ require ("../../../assets/car.png")}/>
 
-          </Marker>
+        </Marker>
+        <Marker 
+                description="Delivery"
+                coordinate={{  latitude: 37.78845,longitude: -122.4340}}>
+                <Image 
+                style={homeStyle.markerImage}
+                source={ require ("../../../assets/car.png")}/>
+
+        </Marker>
+        <Marker 
+                description="Delivery"
+                coordinate={{  latitude: 37.78855,longitude: -122.4355}}>
+                <Image 
+                style={homeStyle.markerImage}
+                source={ require ("../../../assets/car.png")}/>
+
+        </Marker>
+        </>
+        : null
+      }
+
+      { 
+      
+        state == 2 ? 
+
+        <Card>
+
+            <Card.Content>
+                <List.Item 
+                title="15.00 $ "
+                description= " Total Delivery Price "
+                left ={() => 
+                <IconButton
+                icon="car"
+                size={30} />
+                }
+                />
+            </Card.Content>
+        </Card>
+        : null
+
+      }
+           
 
      </MapView>
-     <FAB
-     icon=""
-     style={homeStyle.fab}
-     />
+
+     { 
+     
+        state == 1 ? 
+
+        <FAB
+        icon=""
+        style={homeStyle.fab}
+        />
+        : null
+
+     }
+    
    
    </SafeAreaView>
        
