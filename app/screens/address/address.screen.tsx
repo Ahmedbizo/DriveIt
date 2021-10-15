8334a34830f03f9d1b3896231584f1d4f1f00a25
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-15 23:04:33 +0200

    add address screen to the app

---
+++ app/screens/address/address.screen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { Title } from "react-native-paper";
+
+const AddressScreen = () => {
+
+    return(
+        <SafeAreaView>
+
+            <Title>Address Screen </Title>
+        </SafeAreaView>
+    )
+}
+
+export default AddressScreen;
