991a7cefb35117d7564c66d5299b5383239f498d
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-15 23:05:53 +0200

    added address screen

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { them } from './App.style';
 import { LoginScreen } from './app/screens/login/login.screen';
 import { RegisterScreen } from './app/screens/register/register.screen';
 import HomeScreen from './app/screens/home/home.screen';
+import AddressScreen from './app/screens/address/address.screen';
 
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
   return(
   <PaperProvider theme={them}>
 
-    <HomeScreen/>
+    <AddressScreen/>
   </PaperProvider>
 
   );
