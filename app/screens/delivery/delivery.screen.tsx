2a33bad8a17b389f81be91437a3517b47c812791
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-18 22:07:31 +0200

    added delivery screen to the app

---
+++ app/screens/delivery/delivery.screen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { Title } from 'react-native-paper';
+
+const DeliveryScreen = () => {
+
+
+    return( 
+
+        <SafeAreaView>
+
+            <Title> Delivery Screen </Title>
+        </SafeAreaView>
+    )
+}
+
+export default DeliveryScreen;
