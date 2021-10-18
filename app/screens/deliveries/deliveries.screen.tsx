99458df2ec35dc26d3b07bd2527fecf3fcb08984
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-18 23:58:15 +0200

    added style to My delivery

--- app/screens/deliveries/deliveries.screen.tsx
+++ app/screens/deliveries/deliveries.screen.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Card, Text, Title } from 'react-native-paper';
+import { Avatar, Card, Text } from 'react-native-paper';
 import { HeaderComponent } from '../../components/header/header.component';
+import { DeliveriesSryle } from './deliveries.style';
 
 
 const DeliveriesScreen = () => {
 
     return(
-        <SafeAreaView>
+        <SafeAreaView >
 
             <HeaderComponent title="My Deliveries" hasBackButton={true}/>
-            <Card>
+            <Card style={DeliveriesSryle.card}>
                 <Card.Cover 
                 source={{uri: "https://snazzy-maps-cdn.azureedge.net/assets/74-becomeadinosaur.png?v=20170626082939"}}
                 />
                 <Card.Title 
                     title="18/10/2021"
+                    
                     subtitle="David jon "
+                    
                     right={() => 
-                    <Text> $ 30.00</Text>
-                    }/>
+                    <Text style={DeliveriesSryle.cardText}> $ 30.00</Text>
+                    }
+                    
+                    left={() =>
+                    <Avatar.Image
+                        size={50}
+                        source={{uri : "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"}}
+                        />
+                    }
+                    />
+                    
             </Card>
+            
         </SafeAreaView>
     )
 }
---
+++ app/screens/deliveries/deliveries.style.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const DeliveriesSryle = StyleSheet.create({
+    card: {
+        borderRadius: 30,
+        backgroundColor: "#CDF2CA",
+        margin:8,
+        
+        
+    },
+    cardText: {
+        marginRight:20,
+        fontSize: 30,
+        color: "#09009B"
+       
+        
+    },
+})
