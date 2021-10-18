07cddd0e3f01e6a03a654aa46ff0b844739387f8
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-18 23:45:54 +0200

    added card to deliveries screen

--- app/screens/deliveries/deliveries.screen.tsx
+++ app/screens/deliveries/deliveries.screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Title } from 'react-native-paper';
+import { Card, Text, Title } from 'react-native-paper';
 import { HeaderComponent } from '../../components/header/header.component';
 
 
@@ -10,6 +10,17 @@ const DeliveriesScreen = () => {
         <SafeAreaView>
 
             <HeaderComponent title="My Deliveries" hasBackButton={true}/>
+            <Card>
+                <Card.Cover 
+                source={{uri: "https://snazzy-maps-cdn.azureedge.net/assets/74-becomeadinosaur.png?v=20170626082939"}}
+                />
+                <Card.Title 
+                    title="18/10/2021"
+                    subtitle="David jon "
+                    right={() => 
+                    <Text> $ 30.00</Text>
+                    }/>
+            </Card>
         </SafeAreaView>
     )
 }
