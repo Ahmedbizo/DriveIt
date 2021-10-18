fbce10861fd5c30d662cfb501175c6fe0700a5f5
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-10-18 22:21:02 +0200

    added style to delivery screen

--- app/screens/delivery/delivery.screen.tsx
+++ app/screens/delivery/delivery.screen.tsx
@@ -3,26 +3,30 @@ import { SafeAreaView, View } from 'react-native';
 import MapView from 'react-native-maps';
 
 import { HeaderComponent } from '../../components/header/header.component';
+import { DeliveryStyle } from './delivery.style';
 
 const DeliveryScreen = () => {
 
 
     return( 
 
-        <SafeAreaView>
+        <SafeAreaView style={DeliveryStyle.flex}>
 
             <HeaderComponent title=" Delivery details " hasBackButton={true}/>
-            <View>
+            <View style={DeliveryStyle.flex}>
 
                 <MapView 
                 initialRegion={{
                     latitude: 37.78825,
                     longitude: -122.4324,
-                    latitudeDelta: 0.015,
-                    longitudeDelta: 0.0121,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.04,
+                    
                 }}
+                style={DeliveryStyle.flex}
+                > 
                 
-                ></MapView>
+                </MapView>
             </View>
         </SafeAreaView>
     )
